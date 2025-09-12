c459ea1d717983b59fe95a591939ca4d996f630e
Wilfried <wilfried.mercier58@gmail.com>
2025-09-12 18:18:43 +0200

    Added ClumpFinder to init

---
+++ finder/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#.. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr
+
+from .finder     import ClumpFinder
+from .background import bg_threshold_n_sigma, find_bg_threshold
