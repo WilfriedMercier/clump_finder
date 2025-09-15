f2c227e2f80d1a263a71b943e9fcdee393873302
Wilfried <wilfried.mercier58@gmail.com>
2025-09-15 10:05:28 +0200

    Added detection curves to init

--- finder/__init__.py
+++ finder/__init__.py
@@ -2,5 +2,15 @@
 # -*- coding: utf-8 -*-
 #.. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr
 
-from .finder     import ClumpFinder
-from .background import bg_threshold_n_sigma, find_bg_threshold
+from .finder           import ClumpFinder
+
+from .background       import (
+    bg_threshold_n_sigma, 
+    find_bg_threshold
+)
+
+from .detection_curves import (
+    mag_detection_curve,
+    flux_detection_curve_pixel_level,
+    surface_detection_curve
+)
