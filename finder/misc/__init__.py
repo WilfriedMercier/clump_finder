7c871c0fbe25117fbfe658bd5a5418a3064d3015
Wilfried <wilfried.mercier58@gmail.com>
2025-09-15 11:26:54 +0200

    Added an init file for this submodule

---
+++ finder/misc/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#.. codeauthor:: Wilfried Mercier - LAM <wilfried.mercier@lam.fr
+
+from .misc import (
+    DETECTION_TYPE,
+    generate_bulge_mask,
+    find_neighbours,
+    compute_surface_mask
+)
